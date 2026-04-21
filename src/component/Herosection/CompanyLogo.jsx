66dbc398f3f61b8023a4ee6822fc6527b8c9db78
maryham1 <maryham4real@gmail.com>
2026-04-21 11:44:09 +0100

    change animation posistion

--- src/component/Herosection/CompanyLogo.jsx
+++ src/component/Herosection/CompanyLogo.jsx
@@ -3,11 +3,11 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 function CompanyLogo() {
   return (
     <div className="hidden relative  items-center justify-center laptop:flex tablet:flex">
-      <div className="absolute w-full h-full bg-radial-[at_center] from-blue-500/30  to-blue-600/5 rounded-full blur-3xl z-10 pointer-events-none"></div>
+      <div className="absolute w-full h-full bg-radial-[at_center] from-blue-500/30  to-blue-600/5 rounded-full blur-3xl z-10 pointer-events-none animate-radial transition-all duration-700 ease-in-out"></div>
       <LazyLoadImage
         src="/solvurix-company-logo.png"
         alt="company-logo"
-        className="laptop:w-[600px] laptop:h-[600px] tablet:w-[400px] tablet:w-[400px] relative z-10 "
+        className="laptop:w-[600px] laptop:h-[600px] tablet:w-[400px] tablet:w-[400px] relative z-10 animate-float1 transition-all duration-700 ease-in-out duration-700 "
       />
     </div>
   );
--- src/component/Product/ProductImage.jsx
+++ src/component/Product/ProductImage.jsx
@@ -3,7 +3,7 @@ import "react-lazy-load-image-component/src/effects/blur.css";
 function ProductImage() {
   return (
     <div className="z-20 order-2 laptop:order-1 ">
-      <div className="animate-float1 transition-all duration-700 ease-in-out duration-700">
+      <div className="">
         <LazyLoadImage
           src="\product.jpeg"
           className=" w-[400px] h-[150px] rounded-2xl shadow-xl shadow-gray-300 object-cover hover:scale-[1.05] laptop:w-[600px] laptop:h-[350px] tablet:w-[600px] tablet:h-[250px]   "
